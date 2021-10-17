9d98946f65e2922dfa746042e6d7cd79dda84f71
TechHog <77560013+TechHog8984@users.noreply.github.com>
2021-10-17 16:15:34 -0400

    Create example1.lua

---
+++ example1.lua
@@ -0,0 +1,48 @@
+local lib = require('lines-lib') --here you should do something like loadstring(game:HttpGet('url'))();
+								                 --url being the raw url to the lines-lib file 
+								                 --(go back to the repo page, click lines-lib.lua, and then click raw)
+
+
+local gui = lib:CreateGui('Awesome GUI')
+
+local section1 = gui:CreateSection('Section1')
+
+local button1 = nil
+local label1 = nil
+button1 = section1:TextButton('Button1', function()
+    coroutine.wrap(function()
+        local originalBackgroundColor = button1:GetProperty('BackgroundColor')
+        print('making text invisible')
+        button1:SetProperty('TextVisible', false)
+        wait(1)
+        print('making text visible')
+        button1:SetProperty('TextVisible', true)
+        wait(1)
+        print('setting backgroundcolor to pink')
+        button1:SetProperty('BackgroundColor', Color3.fromRGB(255, 0, 150))
+        wait(1)
+        print('setting backgroundtransparency to 0')
+        button1:SetProperty('BackgroundTransparency', 0)
+        wait(1)
+        print('setting backgroundtransparency to 1')
+        button1:SetProperty('BackgroundTransparency', 1)
+        wait(1)
+        print('setting backgroundcolor to original')
+        button1:SetProperty('BackgroundColor', originalBackgroundColor)
+    end)()
+    coroutine.wrap(function()
+        label1:SetProperty('Text', 'lol')
+        wait(1)
+        label1:SetProperty('Text', 'label1')
+        wait(1)
+        label1:SetProperty('TextTransparency', .5)
+        wait(1)
+        label1:SetProperty('TextColor', Color3.new(0, 0, 0))
+        label1:SetProperty('BackgroundColor', Color3.new(1, 1, 1))
+        wait(1)
+        label1:SetProperty('TextColor', Color3.new(1, 1, 1))
+        label1:SetProperty('BackgroundTransparency', 0)
+    end)()
+end)
+
+label1 = section1:TextLabel('Label1')
