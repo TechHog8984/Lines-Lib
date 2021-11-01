4b93e0f2686dc984fae9b7f8ef799cb62f7aec66
TechHog <77560013+TechHog8984@users.noreply.github.com>
2021-11-01 19:10:05 -0400

    Update lines-lib.lua

--- lines-lib.lua
+++ lines-lib.lua
@@ -1,3 +1,7 @@
---[[
-
+message = [[
+This script is coming soon.
+Yes, the script is fully functional and everything described on the github is accurate.
+The script is just currently not released and private.
 ]]
+
+return message
