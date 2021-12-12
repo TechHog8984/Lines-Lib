74533e59cf94aa9ef004e940cfb0227e33318046
TechHog <77560013+TechHog8984@users.noreply.github.com>
2021-12-12 23:49:42 +0000

    Update lines-lib.lua

--- lines-lib.lua
+++ lines-lib.lua
@@ -1,2 +1,1 @@
---WARNING!!! The file below is very big.
-return loadstring(game:HttpGet('https://download1485.mediafire.com/zbul422qnxfg/zlpc96k1ihhc002/output%283%29.txt'))()
+--not released yet :/
